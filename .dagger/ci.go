61a0efa2890ffcc8b6771f95241fdf02ed6c2c30
Johannes Stang <tubenhirn@gmail.com>
2023-03-04 18:06:13 +0100

    feat: rework semantic-release with go-sdk

---
+++ .dagger/ci.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"dagger.io/dagger"
+	"github.com/tubenhirn/dagger-ci-modules/v2/semanticrelease"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+
+	defer client.Close()
+
+	platform := flag.String("platform", "", "the string of the platform to run on.")
+	flag.Parse()
+
+	fmt.Println("running with flags:", "\nplatform", *platform)
+
+	var secrets = make(map[string]dagger.SecretID)
+	switch *platform {
+	case "github":
+		githubTokenId, err := client.Host().EnvVariable("GITHUB_TOKEN").Secret().ID(ctx)
+		if err != nil {
+			panic(err)
+		}
+		secrets["GITHUB_TOKEN"] = githubTokenId
+	case "gitlab":
+		gitlabTokenId, err := client.Host().EnvVariable("GITLAB_TOKEN").Secret().ID(ctx)
+		if err != nil {
+			panic(err)
+		}
+		secrets["GITLAB_TOKEN"] = gitlabTokenId
+	case "git":
+		githubTokenId, err := client.Host().EnvVariable("GITHUB_TOKEN").Secret().ID(ctx)
+		if err != nil {
+			panic(err)
+		}
+		secrets["GITHUB_TOKEN"] = githubTokenId
+	default:
+		error := errors.New("flag platform missing.")
+		panic(error)
+	}
+
+	dir, _ := os.Getwd()
+
+	options := semanticrelease.SemanticOpts{
+		Source:   dir,
+		Platform: *platform,
+		Env:      map[string]string{},
+		Secret:   secrets,
+	}
+
+	if err := semanticrelease.Semanticrelease(context.Background(), *client, options); err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ semanticrelease/semanticrelease.go
@@ -0,0 +1,82 @@
+package semanticrelease
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"dagger.io/dagger"
+)
+
+type SemanticOpts struct {
+	Source   string
+	Platform string
+	Env      map[string]string
+	Secret   map[string]dagger.SecretID
+}
+
+type image struct {
+	Name    string
+	Version string
+}
+
+var semanticreleaseGithubImage = image{
+	Name: "tubenhirn/semantic-release-github",
+	//# renovate: datasource=docker depName=tubenhirn/semantic-release-github versioning=docker
+	Version: "v3.0.0",
+}
+
+var semanticreleaseGitlabImage = image{
+	Name: "tubenhirn/semantic-release-gitlab",
+	//# renovate: datasource=docker depName=tubenhirn/semantic-release-gitlab versioning=docker
+	Version: "v3.0.0",
+}
+
+var semanticreleaseGitImage = image{
+	Name: "tubenhirn/semantic-release-git",
+	//# renovate: datasource=docker depName=tubenhirn/semantic-release-git versioning=docker
+	Version: "v3.0.0",
+}
+
+func Semanticrelease(ctx context.Context, client dagger.Client, opts SemanticOpts) error {
+
+	sourceDir := client.Host().Directory(opts.Source)
+
+	var image string
+	switch opts.Platform {
+	case "github":
+		image = createImageString(semanticreleaseGithubImage)
+	case "gitlab":
+		image = createImageString(semanticreleaseGitlabImage)
+	case "git":
+		image = createImageString(semanticreleaseGitImage)
+	default:
+		return errors.New("Platform net set.")
+	}
+
+	semanticrelease := client.Container().From(image)
+
+	// write env secrets - access-tokens etc.
+	for key, val := range opts.Secret {
+		semanticrelease = semanticrelease.WithSecretVariable(key, client.Secret(val))
+	}
+
+	// write dditional env variables
+	for key, val := range opts.Env {
+		semanticrelease = semanticrelease.WithEnvVariable(key, val)
+	}
+
+	semanticrelease = semanticrelease.WithMountedDirectory("/src", sourceDir)
+	semanticrelease = semanticrelease.WithWorkdir("/src")
+
+	_, err := semanticrelease.Exec().Stdout(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createImageString(img image) string {
+	return fmt.Sprintf("%s:%s", img.Name, img.Version)
+}
