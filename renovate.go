e635f1e001141af27f52d929fa58aa804f9b2ad7
Johannes Stang <tubenhirn@gmail.com>
2024-06-06 06:49:29 +0200

    chore(deps): update renovate/renovate docker tag to v37.392.0 (#282)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>

--- renovate.go
+++ renovate.go
@@ -22,7 +22,7 @@ type RenovateOpts struct {
 var defaultRenovateImage = Image{
 	Name: "renovate/renovate",
 	//# renovate: datasource=docker depName=renovate/renovate versioning=docker
-	Version: "37.368.9",
+	Version: "37.392.0",
 	Suffix:  "",
 }
 
